925d5f479fff5adae9cc5df339f5d124db6eec30
Maritza Alas <alas.maritza.j@gmail.com>
2017-10-24 00:18:58 +0000

    show page

--- app/controllers/compilations_controller.rb
+++ app/controllers/compilations_controller.rb
@@ -2,4 +2,8 @@ class CompilationsController < ApplicationController
     def index
         @compilations = Compilation.all
     end
+    
+    def show
+        @compilation = Compilation.find(params[:id])
+    end
 end
